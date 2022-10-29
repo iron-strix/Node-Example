a2aaa02aa441b2d881a3edadcdd62aa151e1aefb
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-10-29 03:13:44 -0500

    added error handling for 404, example env

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import cors from 'cors'
 import helmet from 'helmet'
 import logger from './helpers/logger'
 import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
-app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN }))
-app.use(helmet())
+app.use(morgan(process.env.MORGAN_LOG)) // logging
+app.use(cors({ origin: process.env.ORIGIN })) // cross origin
+app.use(helmet()) // secures headers
 
 app.use(router)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () =>
   logger.info(`Application started at http://localhost:${process.env.PORT}`),
