529ebb1939631c3d275191fb20b663da62c3eb63
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-11-05 16:11:11 -0500

    basic authorization added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
+import basicAuth from 'express-basic-auth'
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -13,6 +14,11 @@ const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
+app.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+)
 app.use(morgan(process.env.MORGAN_LOG)) // logging
 app.use(cors({ origin: process.env.ORIGIN })) // cross origin
 app.use(helmet()) // secures headers
