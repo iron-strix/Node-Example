06068fcc019d339cb75d2bf521b0452b1356a5e5
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-11-20 22:00:41 -0600

    removed quotes from schema

---
+++ prisma/migrations/20221121035904_added_missing_employees_fields/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quote` on the `Employee` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Employee" DROP COLUMN "quote";
